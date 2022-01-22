e775953d6ccef511be2b349757ade1b5ccd712b3
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-01-22 17:08:23 +0300

    remove bad ')'

--- new_block.js
+++ new_block.js
@@ -14,7 +14,7 @@ rl.question("Введите название блока ", function(name) {
         fs.mkdirSync(`./app/blocks/${name}`);
         fs.writeFileSync(`./app/blocks/${name}/${name}.js`, `console.log("${name}")`);
         fs.writeFileSync(`./app/blocks/${name}/${name}.scss`, `@import "../../assets/styles/_block_dependencies";\n\n.${name}{\n\t$b: &;\n}`);
-        fs.writeFileSync(`./app/blocks/${name}/${name}.pug`, `mixin(data)\n\t.${name})`);
+        fs.writeFileSync(`./app/blocks/${name}/${name}.pug`, `mixin(data)\n\t.${name}`);
         rl.close();
     }
 });
